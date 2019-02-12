771f9a9c9982f538ca9c0d3ce2d6b88f00ab7721
Ben Minerd <ben.minerd@gmail.com>
2019-02-11 22:23:32 -0500

    Added error return to run_ci.sh.

--- run_ci.sh
+++ run_ci.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -e
+
 if [ ! -d "gcc-arm-none-eabi-7-2017-q4-major" ]; then
     wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2
 
